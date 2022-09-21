8d29a4544476105abe4bcb26834f97c5f0a9b073
Mariia <mashulkaegor@mail.ru>
2022-09-22 01:13:48 +0400

    add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2. Показать натуральные числа от M до N, N и M заданы
+Console.WriteLine("Введите значение M: ");
+int M = int.Parse (Console.ReadLine());
+Console.WriteLine("Введите значение N: ");
+int N = int.Parse (Console.ReadLine());
+int count = M;
+
+while (count <= N)
+{
+    Console.WriteLine(count);
+    count = count + 1;
+}
