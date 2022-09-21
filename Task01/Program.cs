37dcedc661d259cd30f0d94c801150961ff201b4
Mariia <mashulkaegor@mail.ru>
2022-09-22 01:07:42 +0400

    add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1. Показать натуральные числа от N до 1, N задано
+Console.WriteLine("Введите значение N: ");
+int N = int.Parse (Console.ReadLine());
+int count = N;
+
+while (count < 2)
+{
+    Console.WriteLine(count);
+    count = count + 1;
+}
+while (count > 2)
+{
+    Console.WriteLine("Введите другое значение N");
+    break;
+}
