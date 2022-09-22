719010a1559f623360d9d385d774571d4efac8b4
Mariia <mashulkaegor@mail.ru>
2022-09-22 11:17:22 +0400

    add Task06

---
+++ Task06/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 6. Написать программу возведения числа А в целую стень B
+Console.Write("Введите значение a = ");
+int a = int.Parse(Console.ReadLine()); 
+Console.Write("Введите значение b = ");
+int b = int.Parse(Console.ReadLine()); 
+int counter = 1;
+int pow = 1;
+while (counter <= b)
+    { 
+    pow = pow * a;
+    counter++;
+    }
+Console.Write("Число a в степени b " + " равно " + pow);
