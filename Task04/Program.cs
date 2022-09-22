073b914e7b1ea9e3355be94d7bbe144d00bcc891
Mariia <mashulkaegor@mail.ru>
2022-09-22 10:51:09 +0400

    add Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4. Найти сумму цифр числа
+Console.WriteLine("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+if(n >= 100 & n <= 1000)
+{
+    int last = n % 10;
+    int first = n / 100;
+    int second = (n / 10) % 10;
+}
+int sum = (n / 100) + (n / 10) % 10 + (n % 10);
+Console.WriteLine("Сумма цифр числа: " + sum);
+
